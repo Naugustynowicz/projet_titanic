0f847508a0d1819b3d5290f2042c311c0c973997
Nicolas Augustynowicz <nicolas.augustynowicz@gmail.com>
2023-05-25 15:34:56 +0200

    include moyenne

--- barGraph.js
+++ barGraph.js
@@ -1,70 +1,72 @@
 /* eslint-disable no-undef */
 
 // Fonction pour créer le graphique en barre
-function createBarChart(labels, data) {
-    const ctx = document.getElementById('barChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    data: data,
-                    backgroundColor: ['green', 'red'],
-                }
-            ]
+function createBarChart(labels, data, canvas) {
+  const ctx = document.getElementById(canvas).getContext("2d")
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: ["green", "red"],
         },
-        options: {
-
-        }
-    });
+      ],
+    },
+    options: {},
+  })
 }
 
 // Écouteur d'événement pour le formulaire de recherche
-const searchForm = document.getElementById('searchForm');
-searchForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
+const searchForm = document.getElementById("searchForm")
+searchForm.addEventListener("submit", async function (event) {
+  event.preventDefault()
 
-    try {
-        const sexe = document.getElementById('sexe').value;
-        const age = document.getElementById('age').value;
-        const classe = document.getElementById('classe').value;
+  try {
+    const sexe = document.getElementById("sexe").value
+    const age = document.getElementById("age").value
+    const classe = document.getElementById("classe").value
 
-        console.log('Valeurs du formulaire :', sexe, age, classe);
+    console.log("Valeurs du formulaire :", sexe, age, classe)
 
-        // J'envoie la requête POST directement au routeur côté serveur
-        const response = await fetch('/results', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                sexe: sexe,
-                age: age,
-                classe: classe
-            })
-        });
+    // J'envoie la requête POST directement au routeur côté serveur
+    const response = await fetch("/results", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sexe: sexe,
+        age: age,
+        classe: classe,
+      }),
+    })
 
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Données récupérées :', data);
+    if (response.ok) {
+      const data = await response.json()
+      console.log("Données récupérées :", data)
 
-            // J'utilise les données récupérées pour créer le graphique en barre
-            if (data.length > 0) {
-                const labels = ['Survivants', 'Non-survivants'];
-                const survivorsCount = data.filter(passenger => passenger.Survived === 1).length;
-                const nonSurvivorsCount = data.filter(passenger => passenger.Survived === 0).length;
-                const chartData = [survivorsCount, nonSurvivorsCount];
+      // J'utilise les données récupérées pour créer le graphique en barre
+      if (data.length > 0) {
+        const labels = ["Survivants", "Non-survivants"]
+        const survivorsCount = data.filter(
+          (passenger) => passenger.Survived === 1
+        ).length
+        const nonSurvivorsCount = data.filter(
+          (passenger) => passenger.Survived === 0
+        ).length
+        const chartData = [survivorsCount, nonSurvivorsCount]
 
-                createBarChart(labels, chartData);
-            } else {
-                // Aucun passager correspondant aux critères de recherche
-                // Afficher un message approprié ou effectuer une autre action
-            }
-        } else {
-            throw new Error('Erreur lors de la récupération des passagers.');
-        }
-    } catch (error) {
-        console.error(error);
+        createBarChart(labels, chartData)
+      } else {
+        // Aucun passager correspondant aux critères de recherche
+        // Afficher un message approprié ou effectuer une autre action
+      }
+    } else {
+      throw new Error("Erreur lors de la récupération des passagers.")
     }
-});
+  } catch (error) {
+    console.error(error)
+  }
+})
--- routers/router.js
+++ routers/router.js
@@ -1,55 +1,73 @@
 import { Router } from "express"
-import Passenger from "../models/Passenger.js";
-
+import Passenger from "../models/Passenger.js"
 
 const appRouter = new Router()
 
 // Route vers la page de connexion
 appRouter.get("/login", (req, res) => {
-    res.render("login");
-});
+  res.render("login")
+})
 
 // Route vers la page de recherche
-appRouter.get('/search', (req, res) => {
-    res.render('search');
-});
+appRouter.get("/search", (req, res) => {
+  res.render("search")
+})
 
 appRouter.post("/results", async (req, res) => {
-    const { sexe, age, classe } = req.body;
+  const { sexe, age, classe } = req.body
 
-    try {
-        // Récupérer les données de la base de données en fonction des critères de recherche
-        const passengers = await Passenger.find({
-            Sex: sexe,
-            Age: age,
-            Pclass: classe,
-        });
+  try {
+    // Récupérer les données de la base de données en fonction des critères de recherche
+    const passengers = await Passenger.find({
+      $or: [{ Sex: sexe }, { Age: age }, { Pclass: classe }],
+    }).then((passengers) => {
+      console.log(passengers)
+      // Vérifier si des passagers ont été trouvés
+      if (passengers.length > 0) {
+        // Calculer le nombre de survivants et de non-survivants
+        const survivorsCount = passengers.filter(
+          (passenger) => passenger.Survived === true
+        ).length
+        const nonSurvivorsCount = passengers.filter(
+          (passenger) => passenger.Survived === false
+        ).length
 
-        // Vérifier si des passagers ont été trouvés
-        if (passengers.length > 0) {
-            // Calculer le nombre de survivants et de non-survivants
-            const survivorsCount = passengers.filter(passenger => passenger.Survived === 1).length;
-            const nonSurvivorsCount = passengers.filter(passenger => passenger.Survived === 0).length;
-
-            // Créer les données pour le graphique
-            const chartData = [survivorsCount, nonSurvivorsCount];
-
-            // Render la page des résultats en passant les données au template Pug
-            res.render("results", { passengers, chartData });
-        } else {
-            // Aucun passager ne correspond à la recherche
-            res.render("results", { passengers: [], chartData: [] });
+        const survivors = passengers.filter(
+          (passenger) => passenger.Survived === true
+        )
+        const nonSurvivors = passengers.filter(
+          (passenger) => passenger.Survived === false
+        )
+        var avgSurvivors = 0
+        for (var i = 0; i < survivorsCount; i++) {
+          avgSurvivors += survivors[i].Age
         }
-    } catch (error) {
-        console.error("Erreur lors de la recherche des passagers :", error);
-        // Gérez les erreurs appropriées
-    }
-});
+        avgSurvivors /= survivorsCount
 
+        var avgNonSurvivors = 0
+        for (var i = 0; i < nonSurvivorsCount; i++) {
+          avgNonSurvivors += nonSurvivors[i].Age
+        }
+        avgNonSurvivors /= nonSurvivorsCount
 
+        //ecartType = sqrt(avg((x - avg(x)) ^ 2))
+        // Créer les données pour le graphique
+        const chartData = [survivorsCount, nonSurvivorsCount]
+        const chartData2 = [avgSurvivors, avgNonSurvivors]
+        console.log("survivors : " + survivorsCount + " / " + nonSurvivorsCount)
 
-
-
+        // Render la page des résultats en passant les données au template Pug
+        res.render("results", { passengers, chartData, chartData2 })
+      } else {
+        // Aucun passager ne correspond à la recherche
+        res.render("results", { passengers: [], chartData: [] })
+      }
+    })
+  } catch (error) {
+    console.error("Erreur lors de la recherche des passagers :", error)
+    // Gérez les erreurs appropriées
+  }
+})
 
 // appRouter.post("/results", async (req, res) => {
 //     const { sexe, age, classe } = req.body;
@@ -71,5 +89,4 @@ appRouter.post("/results", async (req, res) => {
 //     }
 // });
 
-
-export default appRouter;
+export default appRouter
